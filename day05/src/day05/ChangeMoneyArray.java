b42dd906427a2fef8598d5547d71f3ac0504fbbc
cocoa8192 <cocoa8192@kbu.ac.kr>
2023-05-01 17:08:24 +0900

    Test

--- day05/src/day05/ChangeMoneyArray.java
+++ day05/src/day05/ChangeMoneyArray.java
@@ -2,6 +2,7 @@ package day05;
 
 public class ChangeMoneyArray {
 	public static void main(String[] args) {
+		// 배열은 규칙이 없는 것에 규칙을 만들어 줄 때 사용한다
 		int units [] = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
 		int money = 1876540;
 		for(int i=0; i<units.length; i++) {
